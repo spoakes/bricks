29ffc1c8f64ff6a7fd88b74b5ddcb43388f441aa
Simon Oakes <simon.p.oakes@btinternet.com>
2017-12-17 16:59:20 +0000

    REMOVE commented out code

--- src/main/java/order/Response.java
+++ src/main/java/order/Response.java
@@ -6,11 +6,6 @@ public abstract class Response
 {
 	public final ArrayList<String> errors = new ArrayList<String>();
 
-//	public Response()
-//	{
-//		this.errors = new String[]{};
-//	}
-
 	public void addError(String error)
 	{
 		errors.add(error);
